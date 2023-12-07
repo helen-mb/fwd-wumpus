59afabefacec8d800fbd60da776d47ca00df08fc
Helen Burger <burger.helen.m@gmail.com>
2023-12-07 14:45:33 -0800

    Add functionality to intro & instructions btns

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
 'use strict,'
 // --------------------------------------------------------------------------------
-//Collect Elements
-
+//Collect Elements-----------------------------------------------------------------
+//Game Screens
+const $introductionScreen = $('.introduction-screen');
+const $instructionsScreen = $('.instructions-screen');
+const $gameBoardScreen = $('.game-board-screen');
+const $eventScreen = $('.event-screen');
+const $gameEndScreen = $('.game-end-screen');
+//Interactive Components
+const $viewInstructionsBtn = $('.view-instructions-btn');
+const $enterBtn = $('.enter-btn');
+const $exitInstructionsBtn = $('.exit-instructions-btn');
 //END of "Collect Elements"
 
 
 
 // --------------------------------------------------------------------------------
-//Collect Classes -----------------------------------------------------------------
+//Collect Classes------------------------------------------------------------------
 
 //END of "Collect Classes"
 
@@ -15,7 +24,18 @@
 
 // --------------------------------------------------------------------------------
 //Add Event Listeners--------------------------------------------------------------
-
+$viewInstructionsBtn.on('click', function() {
+    $introductionScreen.hide();
+    $instructionsScreen.show();
+})
+$exitInstructionsBtn.on('click', function() {
+    $instructionsScreen.hide();
+    $introductionScreen.show();
+})
+$enterBtn.on('click', function() {
+    $introductionScreen.hide();
+    $gameBoardScreen.show();
+})
 //END of "Add Event Listeners"
 
 
